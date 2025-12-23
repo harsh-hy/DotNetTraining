31faebd66715a084ae2762c17c40623c917e1c3f
harsh <harshyadav02266@gmail.com>
2025-12-23 11:30:59 +0530

    adding some new  files

---
+++ day2Cond-Loops/loops/PascalTriangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class PascalsTriangle
+{
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int space = 0; space < n - i; space++)
+                Console.Write(" ");
+
+            int val = 1; 
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(val + " ");
+                val = val * (i - j) / (j + 1);
+            }
+            Console.WriteLine();
+        }
+    }
+}
