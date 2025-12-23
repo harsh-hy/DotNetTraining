827aa7647e8487b95adf001810f43dec7d73377c
harsh <harshyadav02266@gmail.com>
2025-12-23 11:22:06 +0530

    adding day3 files

---
+++ day3-OOPS/ConstructorClass/Program.cs
@@ -0,0 +1,30 @@
+﻿﻿namespace ConstructorClass
+{
+    public class Student
+    {
+        public int Id;
+        public string Name;
+
+        // Public constructor
+        public Student(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Name: " + Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(12220839, "Harsh");
+            s.Display();
+        }
+    }
+}
