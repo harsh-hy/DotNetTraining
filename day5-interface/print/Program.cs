c1d96dc3d4b95c018e31879376aca1a5bf182547
harsh <harshyadav02266@gmail.com>
2025-12-24 09:54:36 +0530

    day 5 1

---
+++ day5-interface/print/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OopsSession
+{
+    public interface IPrint
+    {
+        void Print();
+    }
+    public class Document : IPrint
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing document...");
+        }
+    }
+    public class Photo : IPrint
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing photo...");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            IPrint doc = new Document();
+            IPrint photo = new Photo();
+
+            doc.Print();
+            photo.Print();
+        }
+    }
+}
