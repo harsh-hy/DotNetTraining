4c1cafdc92417a5aae9319e4881b79db0195825d
harsh <harshyadav02266@gmail.com>
2025-12-24 15:53:16 +0530

    added something to day 4

---
+++ day4-Abstract/abstractBasic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+abstract class Payment
+{
+    public decimal Amount { get; }
+
+    protected Payment(decimal amount)
+    {
+        Amount = amount;
+    }
+
+    public void PrintReceipt()
+    {
+        Console.WriteLine($"Receipt: Amount = {Amount}");
+    }
+
+    public abstract void Pay();
+}
+
+class UpiPayment : Payment
+{
+    public string UpiId { get; }
+
+    public UpiPayment(decimal amount, string upiId) : base(amount)
+    {
+        UpiId = upiId;
+    }
+
+    public override void Pay()
+    {
+        Console.WriteLine($"Paid {Amount} via UPI ({UpiId}).");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Payment p = new UpiPayment(999.00m, "harshyadav02266@hdfc");
+        p.Pay();
+        p.PrintReceipt();
+    }
+}
