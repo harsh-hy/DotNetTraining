63716627e8876dfe750a82d76bc3b30c47f6e3f3
harsh <harshyadav02266@gmail.com>
2026-01-06 16:10:59 +0530

    Added deligate!!

---
+++ day11-XML/Delegation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Delegation
+{
+    public delegate int DelegatFuncion(int a, int b);
+    public class ExampleOfDelegate
+    {
+        public int a;
+        public int b;
+        public void DelegateExi() // Example method to demonstrate delegate usage
+        {
+            // Instantiate delegates pointing to Add and Subtract methods
+            DelegatFuncion delAdd = new DelegatFuncion(Add); 
+            DelegatFuncion delSub = new DelegatFuncion(Subtract);
+            Console.WriteLine("Addition: " + delAdd(10,5));
+            Console.WriteLine("Subtraction: " + delSub(15,5));
+        }
+        private int Add(int a, int b) // Method to add two integers with an additional constant
+        {
+            return a + b + 10;
+        }
+        private int Subtract(int a, int b)// Method to subtract two integers with an additional constant
+        {
+            return a - b - 5;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleOfDelegate example = new ExampleOfDelegate(); // Create an instance of ExampleOfDelegate
+            example.DelegateExi();
+        }
+    }
+}
