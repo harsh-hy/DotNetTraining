ce4cdae681432484f917a2ec21d6d7e4b0bb0f78
Harsh Yadav <harshyadav02266@gmail.com>
2026-01-16 12:11:43 +0530

    added soln of q7

---
+++ day17-practiceQuestions/ConstructionEstimate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Construction
+{
+    class EstimateDetails
+    {
+        public float ConstructionArea{get; set;}
+        public float SiteArea{get; set;}
+    }
+    public class ConstructionEstimateException:Exception
+    {
+        public ConstructionEstimateException(string message):base(message){ }
+    }
+    class Program
+    {
+        public static EstimateDetails ValidateConstructionEstimate(float constructionArea, float siteArea)
+        {
+            if(constructionArea>siteArea)
+            {
+                throw new ConstructionEstimateException("Sorry your Construction area is not approved");
+            }
+            EstimateDetails ED = new EstimateDetails();
+            ED.ConstructionArea=constructionArea;
+            ED.SiteArea=siteArea;
+            return ED;
+        }
+        public static void Main()
+        {
+            EstimateDetails est=new EstimateDetails();
+            Console.WriteLine("Enter the Construction Area");
+            est.ConstructionArea=Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter the Site Area");
+            est.SiteArea=Convert.ToSingle(Console.ReadLine());
+            try
+            {
+                ValidateConstructionEstimate(est.ConstructionArea,est.SiteArea);
+                Console.WriteLine("Your Construction area is Approved");
+            }
+            catch(ConstructionEstimateException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
