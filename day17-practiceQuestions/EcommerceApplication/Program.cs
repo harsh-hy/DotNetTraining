9c67ae0027596de83864d190cb7ef4b65406d3ee
Harsh Yadav <harshyadav02266@gmail.com>
2026-01-16 11:12:10 +0530

    added soln of  q5

---
+++ day17-practiceQuestions/EcommerceApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Ecom
+{
+    class EcommerceShop
+    {
+        public string UserName{get;set;}
+        public double WalletBalance{get;set;}
+        public double TotalPurchaseAmount{get;set;}
+    }
+    public class InsufficientBalanceException:Exception
+    {
+        public InsufficientBalanceException(string? message):base(message) { }
+    }
+    class Program
+    {
+        public static EcommerceShop MakePayment(string name, double balance,double amount)
+        {
+            if(balance<amount)
+            {
+                throw new InsufficientBalanceException("Insufficient balance in the account");
+            }
+            EcommerceShop ec = new EcommerceShop();
+            ec.UserName = name;
+            ec.WalletBalance = balance;
+            ec.TotalPurchaseAmount=amount;
+            return ec;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Enter the User Name");
+            string? name=Console.ReadLine();
+            Console.WriteLine("Enter the User Balance");
+            double balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Amount");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            try{
+            EcommerceShop result=MakePayment(name,balance,amount);
+            if(result!=null)
+            {
+                Console.WriteLine("Payment Successful");
+                Console.WriteLine($"User Name : {name}\nWallet Balance : {balance}\nTotal Purchase Amount : {amount}\nRemaining Balance :{balance-amount}");
+            }
+            }
+            catch(InsufficientBalanceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ day17-practiceQuestions/YogaMeditation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generics;
+namespace YogaMeditation
+{
+    class MeditationCenter
+    {
+        public int MemberId {get; set;}
+        public int Age {get; set;}
+        public double Weight {get; set;}
+        public double Height {get; set;}
+        public string? Goal {get; set;}
+        public double BMI {get; set;}
+    }
+    class Program
+    {
+        public static ArrayList memberlist =  new ArrayList();
+        public void AddYogaMember(int memberId,int age,double weight,double height,string goal,double bmi)
+        {
+            memberlist.Add(new MeditationCenter{MemberId=memberId,Age=age,Weight=weight,Height=height,Goal=goal,BMI=bmi});
+        }
+        public double CalculateBMI(int memberId)
+        {
+            foreach(MeditationCenter member in memberlist)
+            {
+                if(member.MemberId == memberId)
+                {
+                    double bmi = m.Weight / (m.Height * m.Height);
+                    bmi = Math.Floor(bmi * 100) / 100;
+                    m.BMI = bmi;
+                    return bmi;
+                }
+            }
+            return 0;
+        }
+        
+    }
+}
