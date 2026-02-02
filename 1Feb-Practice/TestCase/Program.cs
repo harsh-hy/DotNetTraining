cc0943f15144ff4a28fa657d4cdc57f008628e79
Harsh Yadav <harshyadav02266@gmail.com>
2026-02-02 15:34:41 +0530

    added testcase

---
+++ 1Feb-Practice/TestCase/Program.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+[TestFixture]
+public class UnitTest
+{
+    [Test]
+    public void Test_Deposit_ValidAmount()
+    {
+        Program account = new Program(100m);
+        account.Deposit(50m);
+        Assert.AreEqual(150m, account.Balance);
+    }
+    [Test]
+    public void Test_Deposit_NegativeAmount()
+    {
+        Program account = new Program(100m);
+        Assert.Throws<Exception>(() => account.Deposit(-20m));
+    }
+    [Test]
+    public void Test_Withdraw_ValidAmount()
+    {
+        Program account = new Program(200m);
+        account.Withdraw(50m);
+        Assert.AreEqual(150m, account.Balance);
+    }
+    [Test]
+    public void Test_Withdraw_InsufficientFunds()
+    {
+        Program account = new Program(100m);
+        Assert.Throws<Exception>(() => account.Withdraw(200m));
+    }
+}
