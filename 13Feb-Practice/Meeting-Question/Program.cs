97a4212ad7e1329dc967ad8edc09ed4ef0fb22e8
Harsh Yadav <harshyadav02266@gmail.com>
2026-02-13 16:01:19 +0530

    added questions of 13th feb along with AdoDotnet Practice

---
+++ 13Feb-Practice/Meeting-Question/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+public class Jewellery
+{
+    public string Id { get; set; }
+    public string Type { get; set; }
+    public string Material { get; set; }
+    public int Price { get; set; }
+}
+public class JewelleryUtility
+{
+    public Dictionary<string, string> GetJewelleryDetails(string id)
+    {
+        Dictionary<string, string> result = new Dictionary<string, string>();
+        foreach(var item in Program.jewelleryDetails)
+        {
+            if(item.Value.Id == id){
+                string value=item.Value.Type+"-"+item.Value.Material;
+                result.Add(id,value);
+                return result;
+            }
+        }
+        return result;
+    }
+    public Dictionary<string, Jewellery> UpdateJewelleryPrice(string id, int price)
+    {
+        Dictionary<string, Jewellery> result = new Dictionary<string, Jewellery>();
+        foreach (var item in Program.jewelleryDetails)
+        {
+            if (item.Value.Id == id)
+            {
+                item.Value.Price = price;
+                result.Add(id, item.Value);
+                return result;
+            }
+        }
+        return result;
+    }
+}
+public class Program
+{
+    public static Dictionary<int, Jewellery> jewelleryDetails = new Dictionary<int, Jewellery>()
+        {
+            {1, new Jewellery{ Id="JW01", Type="Chain", Material="Gold", Price=7985}},
+            {2, new Jewellery{ Id="JW02", Type="Ring", Material="Gold", Price=9335}},
+            {3, new Jewellery{ Id="JW03", Type="Necklace", Material="Gold", Price=8318}}
+        };
+    public static void Main(string[] args)
+    {
+        JewelleryUtility util = new JewelleryUtility();
+        int choice = 0;
+        while (choice!=3)
+        {
+            Console.WriteLine("1.Get Jewellery Details");
+            Console.WriteLine("2.Update Price");
+            Console.WriteLine("3.Exit");
+            Console.Write("Enter your choice");
+            choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                Console.WriteLine("Enter the jewellery id");
+                string id = Console.ReadLine();
+                var res = util.GetJewelleryDetails(id);
+                if (res.Count == 0)
+                {
+                    Console.WriteLine("Jewellery id not found");
+                }
+                else
+                {
+                    foreach (var item in res)
+                    {
+                        Console.WriteLine(item.Key + "   " + item.Value);
+                    }
+                }
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Enter the jewellery id");
+                string id = Console.ReadLine();
+                Console.WriteLine("Enter the price to be updated");
+                int price = int.Parse(Console.ReadLine());
+                var res = util.UpdateJewelleryPrice(id, price);
+                if (res.Count == 0)
+                {
+                    Console.WriteLine("Jewellery id not found");
+                }
+                else
+                {
+                    foreach (var item in res)
+                    {
+                        Jewellery j = item.Value;
+                        Console.WriteLine("Id: " + j.Id +",Type: " + j.Type +",Material: "+j.Material+",Price: " +j.Price);
+                    }
+                }
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Thank you");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input!!");
+            }
+        } 
+    }
+}
---
+++ AdoNetPractice/Program.cs
@@ -0,0 +1,164 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.Data.SqlClient;
+class Program
+{
+    static void InsertData()
+    {
+        string cs = "Server=localhost\\SQLEXPRESS;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Dept: ");
+        string dept = Console.ReadLine();
+
+        Console.Write("Salary: ");
+        decimal salary = decimal.Parse(Console.ReadLine());
+
+        string sql = "INSERT INTO Employees(FullName,Department,Salary) VALUES(@n,@d,@s)";
+
+        using (var con = new SqlConnection(cs))
+        using (var cmd = new SqlCommand(sql, con))
+        {
+            cmd.Parameters.AddWithValue("@n", name);
+            cmd.Parameters.AddWithValue("@d", dept);
+            cmd.Parameters.AddWithValue("@s", salary);
+
+            con.Open();
+            int rows = cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Rows inserted: " + rows);
+        }
+    }
+    static void ShowData()
+    {
+        string cs = "Server=localhost\\SQLEXPRESS;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";
+        string sql = "SELECT * FROM Employees";
+
+        using (var con = new SqlConnection(cs))
+        using (var cmd = new SqlCommand(sql, con))
+        {
+            con.Open();
+
+            using (var r = cmd.ExecuteReader())
+            {
+                while (r.Read())
+                {
+                    Console.WriteLine(r["EmployeeId"] + " " + r["FullName"]);
+                }
+            }
+        }
+    }
+    static void UpdateSalary()
+    {
+        string cs = "Server=localhost\\SQLEXPRESS;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        Console.Write("Employee Id: ");
+        int id = int.Parse(Console.ReadLine());
+
+        Console.Write("New Salary: ");
+        decimal salary = decimal.Parse(Console.ReadLine());
+
+        string sql = "UPDATE Employees SET Salary=@s WHERE EmployeeId=@id";
+
+        using (var con = new SqlConnection(cs))
+        using (var cmd = new SqlCommand(sql, con))
+        {
+            cmd.Parameters.AddWithValue("@s", salary);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            con.Open();
+            int rows = cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Updated rows: " + rows);
+        }
+    }
+    static void DeleteData()
+    {
+        string cs = "Server=localhost\\SQLEXPRESS;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        Console.Write("Employee Id to delete: ");
+        int id = int.Parse(Console.ReadLine());
+
+        string sql = "DELETE FROM Employees WHERE EmployeeId=@id";
+
+        using (var con = new SqlConnection(cs))
+        using (var cmd = new SqlCommand(sql, con))
+        {
+            cmd.Parameters.AddWithValue("@id", id);
+
+            con.Open();
+            int rows = cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Deleted rows: " + rows);
+        }
+    }
+    static void CountEmployees()
+    {
+        string cs = "Server=localhost\\SQLEXPRESS;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";
+        string sql = "SELECT COUNT(*) FROM Employees";
+
+        using (var con = new SqlConnection(cs))
+        using (var cmd = new SqlCommand(sql, con))
+        {
+            con.Open();
+
+            int total = Convert.ToInt32(cmd.ExecuteScalar());
+
+            Console.WriteLine("Total Employees: " + total);
+        }
+    }
+    static void SearchByDept()
+    {
+        string cs = "Server=localhost\\SQLEXPRESS;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        Console.Write("Enter Department: ");
+        string dept = Console.ReadLine();
+
+        string sql = "SELECT EmployeeId,FullName,Salary FROM Employees WHERE Department=@d";
+
+        using (var con = new SqlConnection(cs))
+        using (var cmd = new SqlCommand(sql, con))
+        {
+            cmd.Parameters.AddWithValue("@d", dept);
+
+            con.Open();
+
+            using (var r = cmd.ExecuteReader())
+            {
+                while (r.Read())
+                {
+                    Console.WriteLine(r["EmployeeId"] + " " + r["FullName"] + " " + r["Salary"]);
+                }
+            }
+        }
+    }
+
+
+    static void Main()
+    {
+        Console.WriteLine("1 Insert");
+        Console.WriteLine("2 Show");
+        Console.WriteLine("3 Update Salary");
+        Console.WriteLine("4 Delete");
+        Console.WriteLine("5 Count Employees");
+        Console.WriteLine("6 Search By Dept");
+        int ch = int.Parse(Console.ReadLine());
+        if (ch == 1) InsertData();
+        else if (ch == 2) ShowData();
+        else if (ch == 3) UpdateSalary();
+        else if (ch == 4) DeleteData();
+        else if (ch == 5) CountEmployees();
+        else if (ch == 6) SearchByDept();
+    }
+
+}
+
+
+
+
