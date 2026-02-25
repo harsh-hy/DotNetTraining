5f5a45e0de21f000fff71142631b2d7217e4fdb0
Harsh Yadav <harshyadav02266@gmail.com>
2026-02-25 09:21:47 +0530

    added employee crud

---
+++ 23Feb-3Tier/Employeee/Employeee/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Employeee.Models;
+
+namespace Employeee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // STATIC COLLECTION 
+        private static List<Employee> empList = new List<Employee>();
+
+        // READ ALL
+        public IActionResult Index()
+        {
+            return View(empList);
+        }
+
+        // CREATE - GET
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        // CREATE - POST
+        [HttpPost]
+        public IActionResult AddEmployee(Employee emp)
+        {
+            var exists = empList.Any(e => e.Id == emp.Id);
+
+            if (exists)
+            {
+                ModelState.AddModelError("", "Employee ID already exists!");
+                return View(emp);
+            }
+
+            empList.Add(emp);
+            return RedirectToAction("Index");
+        }
+
+        // EDIT - GET
+        public IActionResult Edit(int id)
+        {
+            var emp = empList.FirstOrDefault(e => e.Id == id);
+            return View(emp);
+        }
+
+        // EDIT - POST
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            var oldEmp = empList.FirstOrDefault(e => e.Id == emp.Id);
+
+            if (oldEmp != null)
+            {
+                oldEmp.Name = emp.Name;
+                oldEmp.Department = emp.Department;
+                oldEmp.City = emp.City;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // DELETE
+        public IActionResult Delete(int id)
+        {
+            var emp = empList.FirstOrDefault(e => e.Id == id);
+            if (emp != null)
+                empList.Remove(emp);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 23Feb-3Tier/Employeee/Employeee/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Employeee.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Employeee.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View("../Employee/AddEmployee");
+        }
+        //public IActionResult AddEmployee()
+        //{
+        //    return View("../Employee/AddEmployee");
+        //}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
