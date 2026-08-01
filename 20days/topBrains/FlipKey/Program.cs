6cad66bd7804a3515833191f9facae95397746e2
Harsh Yadav <harshyadav02266@gmail.com>
2026-08-01 17:51:28 +0530

    added some topbrains question

---
+++ 20days/topBrains/FlipKey/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;              
+using System.Linq;         
+class FlipKey
+{
+    public static string CleanseAndInvert(string input)
+    {
+        if (string.IsNullOrEmpty(input) || input.Length < 6)
+        {
+            return "";
+        }
+        input = input.ToLower();
+        char[] characters = input.ToCharArray();
+        string filteredString = "";
+        foreach (char ch in characters)
+        {
+            if (!char.IsLetter(ch))
+                return "";
+            int asciiValue = ch;
+            if (asciiValue % 2 != 0)
+                filteredString += ch;
+        }
+        filteredString = new string(filteredString.Reverse().ToArray());
+        char[] result = filteredString.ToCharArray();
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (i % 2 == 0)
+                result[i] = char.ToUpper(result[i]);
+        }
+        return new string(result);
+    }
+    public static void Main()
+    {
+        Console.Write("Enter the string: ");
+        string input = Console.ReadLine();
+        string output = CleanseAndInvert(input);
+        Console.WriteLine("Ans: " + output);
+    }
+}
---
+++ 20days/topBrains/swap2no/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    static void Main()
+    {
+        int x = 10;
+        int y = 20;
+        Console.WriteLine("Before Swap:");
+        Console.WriteLine($"x = {x}, y = {y}");
+        Swap(ref x, ref y);
+        Console.WriteLine("After Swap:");
+        Console.WriteLine($"x = {x}, y = {y}");
+    }
+}
